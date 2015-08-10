645d0100f5cae0d4fbd936dd14bc5324d815ee25
Paul Friederichsen <floogulinc@gmail.com>
2015-08-10 01:13:58 -0500

    Fixes most of /mode

--- src/plugins/inputs/mode.js
+++ src/plugins/inputs/mode.js
@@ -5,26 +5,30 @@ module.exports = function(network, chan, cmd, args) {
 		return;
 	}
 	
-	var mode;
-	var user;
+	var target;
+	var flags;
+	var params;
+
 	if (cmd != "mode") {
-		user = args[0];
-		mode = {
+		params = args[0];
+		flags = {
 		     "op": "+o",
 		  "voice": "+v",
 		   "deop": "-o",
 		"devoice": "-v"
 		}[cmd];
+		target = chan.name;
 	} else if (args.length === 1) {
 		return;
+	} else if((args[0].slice(0, 1) == '+') || (args[0].slice(0, str.length) == '-')) {
+		target = chan.name;
+		flags = args[0];
+		params = args.slice(1).join(" ");
 	} else {
-		mode = args[0];
-		user = args[1];
+		target = args[0];
+		flags = args[1];
+		params = args.slice(2).join(" ");
 	}
 	var irc = network.irc;
-	irc.mode(
-		chan.name,
-		mode,
-		user
-	);
+	irc.mode(target, flags, params);
 };
