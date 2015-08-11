f0c468f0bf45a455e83e943653980e3b39692140
Paul Friederichsen <floogulinc@gmail.com>
2015-08-11 08:18:52 -0500

    Add /umode command for user modes

--- src/plugins/inputs/mode.js
+++ src/plugins/inputs/mode.js
@@ -1,5 +1,5 @@
 module.exports = function(network, chan, cmd, args) {
-	if (cmd != "mode" && cmd != "op" && cmd != "voice" && cmd != "deop" && cmd != "devoice") {
+	if (cmd != "mode" && cmd != "op" && cmd != "voice" && cmd != "deop" && cmd != "devoice" && cmd != "umode") {
 		return;
 	} else if (args.length === 0) {
 		return;
@@ -9,7 +9,7 @@ module.exports = function(network, chan, cmd, args) {
 	var flags;
 	var params;
 
-	if (cmd != "mode") {
+	if (cmd != "mode" && cmd != "umode") {
 		params = args[0];
 		flags = {
 		     "op": "+o",
@@ -18,9 +18,13 @@ module.exports = function(network, chan, cmd, args) {
 		"devoice": "-v"
 		}[cmd];
 		target = chan.name;
+	} else if (cmd == "umode") {
+		target = network.irc.me;
+		flags = args[0];
+		params = args.slice(1).join(" ");
 	} else if (args.length === 1) {
 		return;
-	} else if((args[0].slice(0, 1) == '+') || (args[0].slice(0, 1) == '-')) {
+	} else if ((args[0].slice(0, 1) == '+') || (args[0].slice(0, 1) == '-')) {
 		target = chan.name;
 		flags = args[0];
 		params = args.slice(1).join(" ");
